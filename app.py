88dea3765d3e76f77c4ad7a9650e5799f02a9666
Michael Hunger <github@jexp.de>
2015-06-24 22:46:12 +0200

    added command parsing

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ class index:
 
 class slack:
     def POST(self):
-         print web.data()
-         input = web.input()
-         print input
-         return "It works"
+         data = web.data()
+         text = data["text"]
+         print "text "+text
+         command = text.split(" ")[0]
+#         if (command == "") return  overview()
+#         if (command == "import") return insert()
+#         if (command == "cypher") return cypher(text[7:])
+         return "It works: command: "+command
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
