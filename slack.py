59768404c326f8bd33394bffb78ac07038d9478b
nicolewhite <nicole.white@neotechnology.com>
2015-06-24 14:22:26 -0700

    Bug in insert: parameter needs to be passed as dictionary.

--- slack.py
+++ slack.py
@@ -8,16 +8,20 @@ token = os.environ.get('SLACK_TOKEN')
 graph = Graph(url)
 
 def insert() :
-# request from slack
-    res = requests.get("https://slack.com/api/channels.list?token="+token)    
+    res = requests.get("https://slack.com/api/channels.list?token={}".format(token))
+
     if res.status_code != 200: 
-        raise Exception("Invalid Response from Channels endpoint "+res.status_code)
+        raise Exception(u"Invalid Response from Channels endpoint {}".format(res.status_code))
+
     channels = res.json()['channels']
+
     query = """
-       unwind {channels} as channel
-       merge (c:Channel {id:channel.id}) ON CREATE SET c.name = channel.name
+    UNWIND {channels} AS channel
+    MERGE (c:Channel {id:channel.id})
+    ON CREATE SET c.name = channel.name
     """
-    graph.cypher.execute(query, channels)
+
+    graph.cypher.execute(query, {'channels': channels})
     
 
 if __name__ == "__main__":
